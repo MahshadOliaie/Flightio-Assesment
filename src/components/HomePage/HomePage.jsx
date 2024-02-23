156895a13917d983097433d1a9fb6035072d4cb1
MahshadOliaie <mahshadoliaie@gmail.com>
2024-02-23 22:55:50 +0330

    add search bar

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
 import Header from '../Header/Header'
+import SearchBar from '../SearchBar/SearchBar'
 import CSS from './homePage.module.css'
 
-function HomePage(){
-    return(
+function HomePage() {
+    return (
         <>
-        <Header />
-        <div className={CSS.heading}>
-            <p className={CSS.welcome}>Welcome, Flightio</p>
-            <h1 className={CSS.title}>Bring Designer Lamps This Diwali</h1>
-        </div>
+            <Header />
+            <div className={CSS.heading}>
+                <p className={CSS.welcome}>Welcome, Flightio</p>
+                <h1 className={CSS.title}>Bring Designer Lamps This Diwali</h1>
+            </div>
+
+            <SearchBar />
         </>
     )
 }
