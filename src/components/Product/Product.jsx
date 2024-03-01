a10e33e24ba222d0bffe063c6a570426f568c67d
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-02 00:33:53 +0330

    able to like

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,16 +1,37 @@
+import { useContext } from 'react';
+import HeartSvg from '../HeartSvg/HeartSvg'
 import CSS from './product.module.css'
+import FavoriteItems from '../../context/favoriteItem/FavoriteItem';
 
-function Product({data}){
-    return(
+function Product({ data }) {
+    const { favoriteItems, setFavoriteItems } = useContext(FavoriteItems)
+
+    const isFavorite = favoriteItems.find((item) => item.id === data.id);
+
+    function handleClick() {
+        if (!isFavorite) {
+            setFavoriteItems([...favoriteItems, data])
+        }
+        else {
+            setFavoriteItems(favoriteItems.filter(item => {
+                return item.id !== data.id
+            }))
+        }
+    }
+
+    return (
         <>
-        <div className={CSS.product}>
-            <div className={CSS.image}><img src={data.image} alt="" /></div>
-            <div className={CSS.about}>
-                <h3 className={CSS.title}>{data.title}</h3>
-                <p className={CSS.desc}>category: {data.category}</p>
-                <p className={CSS.price}>{data.price}$</p>
+            <div className={CSS.product}>
+                <div className={CSS.image}><img src={data.image} alt="" /></div>
+                <div className={CSS.about}>
+                    <h3 className={CSS.title}>{data.title}</h3>
+                    <p className={CSS.desc}>category: {data.category}</p>
+                    <div className={CSS.lastLine}>
+                        <p className={CSS.price}>{data.price}$</p>
+                        <HeartSvg onClick={handleClick} fill={isFavorite ? "red" : "rgba(0, 0, 0, 0.306)"} />
+                    </div>
+                </div>
             </div>
-        </div>
         </>
     )
 }
