d4f66a72d0391688555a3b306cc39e33092a172c
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-03 02:10:38 +0330

    add skeleton for detail page

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import useFetch from "../../hooks/useFetch/useFetch"
 import CSS from './index.module.css'
+import DetailSkeleton from "../Skeleton/DetailPageSkeleton/DetailSkeleton"
 
 
 function ProductDetail() {
@@ -9,20 +10,25 @@ function ProductDetail() {
 
     return (
         <>
-            <div className={CSS.detail}>
-                <div className={CSS.image}>
-                    <img src={product.image} alt="" />
+            {(product.length > 0 || product.id) ?
+                <div className={CSS.detail}>
+                    <div className={CSS.image}>
+                        <img src={product.image} alt="" />
+                    </div>
+                    <div className={CSS.about}>
+                        <h2>{product.title}</h2>
+                        <p className={CSS.category}><span>Category: </span>{product.category}</p>
+                        <p >{product.description}</p>
+                        <p className={CSS.price}>{product.price}$</p>
+                    </div>
+                    <div className={CSS.add}>
+                        <p className={CSS.addBtn}>ADD TO BAG</p>
+                    </div>
                 </div>
-                <div className={CSS.about}>
-                    <h2>{product.title}</h2>
-                    <p className={CSS.category}><span>Category: </span>{product.category}</p>
-                    <p >{product.description}</p>
-                    <p className={CSS.price}>{product.price}$</p>
-                </div>
-                <div className={CSS.add}>
-                    <p className={CSS.addBtn}>ADD TO BAG</p>
-                </div>
-            </div>
+                :
+                <DetailSkeleton />
+            }
+
         </>
     )
 }
